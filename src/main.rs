73cebbe9b08613b63347a3dd6ebd099144082aaf
Andreas Larsen <andreas@northcode.no>
2017-12-20 16:13:28 +0100

    multithreading

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
+use std::thread;
 
 use regex::Regex;
 
@@ -21,6 +22,105 @@ enum HttpResult {
 }
 
 use HttpResult::*;
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>
+}
+
+enum Message {
+    NewJob(Job),
+    Terminate
+}
+
+use Message::*;
+
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+type Job = Box<FnBox + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let mut workers = Vec::with_capacity(size);
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where
+        F: FnOnce() + Send + 'static
+    {
+        let job = Box::new(f);
+
+        self.sender.send(NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in &mut self.workers {
+            self.sender.send(Terminate).unwrap();
+        }
+
+        for worker in &mut self.workers {
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let message = receiver.lock().unwrap().recv().unwrap();
+
+
+                match message {
+                    NewJob(job) => {
+                        println!("Got job for worker {}, executing...", id);
+                        job.call_box();
+                    },
+                    Terminate => break,
+                };
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
 
 fn main() {
 
@@ -51,10 +151,20 @@ fn main() {
     
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
+    let pool = ThreadPool::new(4);
+
+    let routes = Arc::new(Mutex::new(routes));
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream, &routes);
+        let routes = Arc::clone(&routes);
+
+        pool.execute(move || {
+            let routes = routes.lock().unwrap();
+            
+            handle_connection(stream, &routes);
+        });
         println!("Connection established!");
     }
 }
@@ -77,9 +187,9 @@ fn serve_file(path: &str) -> HttpResult {
 
 fn render_result(result: HttpResult) -> String {
     match result {
-        HttpOk(response) => format!("HTTP/1.1 200 Ok\n\n{}", response),
-        HttpNotFound(message) => format!("HTTP/1.1 404 Not Found\n\n{}", message),
-        HttpServerError(error) => format!("HTTP/1.1 500 Server Error\n\n{}", error)
+        HttpOk(response) => format!("HTTP/1.1 200 Ok\r\n\r\n{}", response),
+        HttpNotFound(message) => format!("HTTP/1.1 404 Not Found\r\n\r\n{}", message),
+        HttpServerError(error) => format!("HTTP/1.1 500 Server Error\r\n\r\n{}", error)
     }
 }
 
